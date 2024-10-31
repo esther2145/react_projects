3b1c170a9e739aa886f236950e2e4fc75021f187
esther2145 <kicaest03@gmail.com>
2024-10-31 09:52:10 +0300

    update

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -3,6 +3,16 @@ function index() {
     <h1>KFC APPLICATION</h1>
     <p>This is interesting</p>
     <h2>STORY OF MY LIFE</h2>
+    <table>
+      <tr>
+        <th>Countries</th>
+        <th>Cities</th>
+      </tr>
+      <tr>
+        <td>Uganda</td>
+        <td>Kampala</td>
+      </tr>
+    </table>
   </div>
 }
 export default index
